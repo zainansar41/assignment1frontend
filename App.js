93d73263a8ebc26fa5d2b491ca1db3230f8bf168
ZAIN UL ABIDIN <108021130+zainansar41@users.noreply.github.com>
2023-03-18 17:52:25 +0500

    screens basic structure added

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import React from 'react'
+import Login from './Screens/LoginSignup/Login';
+import AddNote from './Screens/AddNote/AddNote';
+import ViewNote from './Screens/ViewNotes/ViewNote';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="login"
+          component={Login}
+          options={{ headerShown: false}}
+        />
+        <Stack.Screen name="addnote"
+          component={AddNote}
+          options={{ headerShown: false}}
+        />
+        <Stack.Screen name="viewNotes"
+          component={ViewNote}
+          options={{ headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
